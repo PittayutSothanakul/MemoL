90f42bf8ac8eaeb4ad49ac38cc454a41c6e6deaa
Pittayut Sothanakul <pittayut.sot@ku.th>
2019-05-10 05:52:47 +0700

    set Date , Time by imageButton

--- app/src/main/java/com/example/memol/AddMemoActivity.java
+++ app/src/main/java/com/example/memol/AddMemoActivity.java
@@ -1,19 +1,29 @@
 package com.example.memol;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -35,7 +45,8 @@ import java.util.Locale;
 public class AddMemoActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private Button selectButton , enterButton;
+    private Button selectButton , enterButton ;
+    private ImageButton dateButton , timeButton;
     private TextView nameText , dateText,timeText , locationText , descriptionText;
     private FirebaseAuth mAuth;
     private Firebase mRootRef;
@@ -53,13 +64,17 @@ public class AddMemoActivity extends AppCompatActivity {
 
         Firebase.setAndroidContext(this);
         //create a date string.
-        String date_n = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
-        Calendar c = Calendar.getInstance(); // Get current time
-        int hr1 = c.get(Calendar.HOUR_OF_DAY);
-        int sec1 =c.get(Calendar.SECOND);
-        int min1 =c.get(Calendar.MINUTE);
+        String date_n = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+        Calendar c1 = Calendar.getInstance(); // Get current time
+        c1.setTimeInMillis(System.currentTimeMillis());
+        int hour = c1.get(Calendar.HOUR);
+        int min = c1.get(Calendar.MINUTE);
+        int sec = c1.get(Calendar.SECOND);
+
         selectButton = (Button) findViewById(R.id.selectButton);
         enterButton = (Button) findViewById(R.id.enterButton);
+        timeButton = (ImageButton) findViewById(R.id.timeButton);
+        dateButton = (ImageButton) findViewById(R.id.dateButton);
         nameText = (TextView) findViewById(R.id.nameText);
         dateText = (TextView) findViewById(R.id.dateText);
         timeText = (TextView) findViewById(R.id.timeText);
@@ -68,9 +83,64 @@ public class AddMemoActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
 
         dateText.setText(date_n);
-        timeText.setText(hr1 +":" +min1 + ":" + sec1);
+        String am_pm;
+        if(hour<12){
+            am_pm = "AM";
+
+        }else{
+            am_pm = "PM";
+        }
+        timeText.setText(hour + ":" + min + " " + am_pm);
 
         mProgressDialog = new ProgressDialog(AddMemoActivity.this);
+
+
+        dateButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Calendar c = Calendar.getInstance(); // Get current time
+                c.setTimeInMillis(System.currentTimeMillis());
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dpd;
+                dpd = new DatePickerDialog(AddMemoActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        dateText.setText(day + "-" + (month+1) + "-" + year);
+                    }
+                }, day, month, year);
+                dpd.show();
+            }
+        });
+
+        timeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Calendar c = Calendar.getInstance(); // Get current time
+                c.setTimeInMillis(System.currentTimeMillis());
+                int hour = c.get(Calendar.HOUR);
+                int min = c.get(Calendar.MINUTE);
+                int sec = c.get(Calendar.SECOND);
+                TimePickerDialog tpd;
+                tpd = new TimePickerDialog(AddMemoActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int min) {
+                        String am_pm;
+                        if(hour<12){
+                            am_pm = "AM";
+
+                        }else{
+                            am_pm = "PM";
+                        }
+                        timeText.setText(hour + ":" + min + " " + am_pm);
+                    }
+                },hour,min,false);
+                tpd.show();
+            }
+        });
+
 
         selectButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,6 +156,7 @@ public class AddMemoActivity extends AppCompatActivity {
                 }
             }
         });
+
 
         // initialize firebase
         mdatabaseRef = FirebaseDatabase.getInstance().getReference();
