f55fdc634320b768c32ccc7f7d9b918a9b85fd9a
Pittayut Sothanakul <pittayut.sot@ku.th>
2019-05-12 05:44:45 +0700

    fig bug when open app

--- app/src/main/java/com/example/memol/MainActivity.java
+++ app/src/main/java/com/example/memol/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,53 +16,53 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private EditText userName, password;
-    private Button bt_login;
-
+    private TextView id , password, txt_register;
+//    private EditText email, password ,txt_register;
+    private Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
 
-        userName = (EditText) findViewById(R.id.userName);
-        password = (EditText) findViewById(R.id.re_password);
-        bt_login = (Button) findViewById(R.id.loginButton);
+        id = (TextView) findViewById(R.id.userName);
+        password = (TextView) findViewById(R.id.re_password);
+        signin = (Button) findViewById(R.id.loginButton);
 
+        // check if user is logged in
         if(mAuth.getCurrentUser() != null) {
             // User not login
             finish();
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
         }
 
-        bt_login.setOnClickListener(new View.OnClickListener() {
+        signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String getemail = userName.getText().toString();
+                String getemail = id.getText().toString();
                 String getpassword = password.getText().toString();
 
-                calllogin(getemail, getpassword);
+                callsignin(getemail, getpassword);
             }
         });
 
     }
 
 
-    public void onClickedRegister(View view){
+        public void onClickedRegister(View view){
         Intent i = new Intent(MainActivity.this, RegisterActivity.class);
         finish();
         startActivity(i);
     }
-
-
-    private void calllogin(String email, String password) {
+    private void callsignin(String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -79,5 +80,65 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
 }
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        FirebaseApp.initializeApp(this);
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        userName = (EditText) findViewById(R.id.userName);
+//        password = (EditText) findViewById(R.id.re_password);
+//        bt_login = (Button) findViewById(R.id.loginButton);
+//
+//        if(mAuth.getCurrentUser() != null) {
+//            // User not login
+//            finish();
+//            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//        }
+//
+//        bt_login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String getemail = userName.getText().toString();
+//                String getpassword = password.getText().toString();
+//
+//                calllogin(getemail, getpassword);
+//            }
+//        });
+//
+//    }
+//
+//
+//    public void onClickedRegister(View view){
+//        Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+//        finish();
+//        startActivity(i);
+//    }
+//
+//
+//    private void calllogin(String email, String password) {
+//        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                Log.d("test", "Sign in is successful: " + task.isSuccessful());
+//
+//                if(!task.isSuccessful()) {
+//                    Log.d("test", "Sign in with email failed: ", task.getException());
+//                    Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
+//                    finish();
+//                    startActivity(i);
+//                }
+//            }
+//        });
+//    }
+//
+//
+//}
